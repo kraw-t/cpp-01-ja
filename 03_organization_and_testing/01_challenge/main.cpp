382fa6a4646d45deb82c99727e92607c7c5f1d9d
tanaka <152840606+kraw-t@users.noreply.github.com>
2023-12-19 15:40:02 +0900

    report: 03-challenge01

---
+++ 03_organization_and_testing/01_challenge/main.cpp
@@ -0,0 +1,11 @@
+// チャレンジ1
+// このプログラムをコンパイルして実行し、エラーの原因となっているコードを修正してください。
+// その際、コードを削除するのではなく、コードを追加して修正してください。
+
+#include <iostream>
+#include "temp_sensor.h"
+
+int main() {
+    std::cout << "Temperature reading: " << Sensor::readTemperature() << std::endl;
+    return 0;
+}
